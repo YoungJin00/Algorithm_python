73703e42926fc9f4f91128ab01a4d49011f03d5a
PYJ <dudwlscjswo95@gmail.com>
2021-11-05 17:53:35 +0900

    2021.11.05 algorithm

--- 6.배열.py
+++ 6.배열.py
@@ -56,22 +56,83 @@
 # print(f'이 가운데 최댓값은 {max_of(x)} 입니다.')
 
 #2-6
-from typing import Any, MutableSequence
+# from typing import Any, MutableSequence
+#
+# def reverse_array(a: MutableSequence) -> None:
+#     n = len(a)
+#     for i in range(n // 2):
+#         a[i], a[n - i -1] = a[n - i -1], a[i]
+#
+# if __name__ == '__main__':
+#     print('배열 원소를 역순으로 정렬합니다.')
+#     nx = int(input('원소 수를 입력하세요 : '))
+#     x = [None] * nx
+#
+#     for i in range(nx):
+#         x[i] = int(input(f'x[{i}]값을 입력하세요 : '))
+#
+#     reverse_array(x)
+#     print('배열 원소를 역순으로 정렬했습니다.')
+#     for i in range(nx):
+#         print(f'x[{i}] = {x[i]}')
 
-def reverse_array(a: MutableSequence) -> None:
-    n = len(a)
-    for i in range(n // 2):
-        a[i], a[n - i -1] = a[n - i -1], a[i]
+#2-8
+#1000이하 소수 나열
 
-if __name__ == '__main__':
-    print('배열 원소를 역순으로 정렬합니다.')
-    nx = int(input('원소 수를 입력하세요 : '))
-    x = [None] * nx
+# counter = 0
+# for n in range(2, 1001):
+#     for i in range(2, n):
+#         counter += 1
+#         if n % i == 0:
+#             break
+#     else:
+#         print(n)
+# print(f'나눗셈을 실행한 횟수 : {counter}')
 
-    for i in range(nx):
-        x[i] = int(input(f'x[{i}]값을 입력하세요 : '))
+#2-9
+# counter = 0
+# ptr = 0 #이미 찾은 소수의 개수
+# prime = [None] * 500
+#
+# prime[ptr] = 2 #2는 소수이므로 초깃값으로 지정
+# ptr += 1
+#
+# for n in range(3, 1001, 2): #홀수만을 대상으로
+#     for i in range(1, ptr): #이미 찾은 소수로 나눔
+#         counter += 1
+#         if n % prime[i] == 0:
+#             break
+#     else:
+#         prime[ptr] = n
+#         ptr += 1
+#
+# for i in range(ptr):
+#     print(prime[i])
+# print(f'나눗셈을 실행한 횟수 : {counter}')
 
-    reverse_array(x)
-    print('배열 원소를 역순으로 정렬했습니다.')
-    for i in range(nx):
-        print(f'x[{i}] = {x[i]}')
+#2-10
+counter = 0
+ptr = 0
+prime = [None] * 500
+
+prime[ptr] = 2
+ptr += 1
+
+prime[ptr] = 3
+ptr += 1
+
+for n in range(5, 1001, 2):
+    i = 1
+    while prime[i] * prime[i] <= n:
+        counter +=2
+        if n % prime[i] == 0:
+            break
+        i += 1
+    else:
+        prime[ptr] = n
+        ptr += 1
+        counter +=1
+
+for i in range(ptr):
+    print(prime[i])
+print(f'나눗셈을 실행한 횟수 : {counter}')
